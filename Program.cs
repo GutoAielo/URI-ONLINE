b4901e22ab9705d64bd7ec891df87b9dbef7aae3
GutoAielo <53352979+GutoAielo@users.noreply.github.com>
2020-03-27 13:26:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i;
+
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 1; i < 10000; i++) {
+                if (i % n == 2) {
+                    Console.WriteLine(i);
+                }
+            }
+            
+                  
+        }
+    }
+}
