6a021cced0adb28453d8cf5099af894ef32a0470
GutoAielo <53352979+GutoAielo@users.noreply.github.com>
2020-03-27 17:23:27 -0300

    1094 C#

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 
 namespace ConsoleApplication1
@@ -7,17 +8,49 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int n,i;
+            int N, coelho, rato, sapo, quantia;
+            char tipo;
+            double pcoelho, prato, psapo;
 
-            n = int.Parse(Console.ReadLine());
+            coelho = 0; rato = 0; sapo = 0;
 
-            for (i = 1; i < 10000; i++) {
-                if (i % n == 2) {
-                    Console.WriteLine(i);
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+                quantia = int.Parse(valores[0]);
+                tipo = char.Parse(valores[1]);
+
+                if (tipo == 'C')
+                {
+                    coelho = coelho + quantia;
+                }
+                else if (tipo == 'R')
+                {
+                    rato = rato + quantia;
+                }
+                else
+                {
+                    sapo = sapo + quantia;
                 }
             }
-            
-                  
+
+            int total = coelho + rato + sapo;
+            pcoelho = (double)coelho / total * 100.0;
+            prato = (double)rato / total * 100.0;
+            psapo = (double)sapo / total * 100.0;
+
+            Console.WriteLine("Total: " + total + " cobaias");
+            Console.WriteLine("Total de coelhos: " + coelho);
+            Console.WriteLine("Total de ratos: " + rato);
+            Console.WriteLine("Total de sapos: " + sapo);
+            Console.WriteLine("Percentual de coelhos: " + pcoelho.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de ratos: " + prato.ToString("F2", CultureInfo.InvariantCulture) + " %");
+            Console.WriteLine("Percentual de sapos: " + psapo.ToString("F2", CultureInfo.InvariantCulture) + " %");
+
+                                  
         }
+
     }
 }
